6d55c3d6d1bae7922f6d02b9a54efc8ef32586e1
Sam Scibetta <sammieeccentric@gmail.com>
2020-10-10 18:37:45 -0400

    i got one score to showthe most recent score shows but the others do not.

--- printscore.js
+++ printscore.js
@@ -1,19 +1,31 @@
 // create print fucntion that gets info from local storage and prints it ot highscores.js
+var resultsEl = document.querySelector("#high-scores");
+var high_scores = localStorage.getItem("scores");
+
+if (!high_scores) {
+    high_scores = [];
+  } else {
+    high_scores = JSON.parse(high_scores);
+  }
+
+
+printScore()
 
 function printScore() {
-
-      var high_scores = localStorage.getItem("scores");
-    
-      high_scores.sort(function (a, b) {
-        return b.score - a.score;
-      })
-      var contentUl = document.createElement("ul");
-    
-      for (i = 0; i < high_scores.length; i++) {
-        var contentLi = document.createElement("li");
+    var contentUl = document.createElement("ul");
+    var contentLi = document.createElement("li");
+    for (i = 0; i < high_scores.length; i++) {
         contentLi.textContent = "Name: " + high_scores[i].name + " Score: " + high_scores[i].score;
         contentUl.append(contentLi);
-      }
-    document.getElementById("#high-scores").append(contentUl);
+        console.log(contentUl)
+    }
+    high_scores.sort(function (a, b) {
+        return b.score - a.score;
+    })
 
-  }
+
+    resultsEl.append(contentUl);
+    console.log(contentUl)
+
+}
+
